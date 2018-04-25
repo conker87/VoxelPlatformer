a403dc8c0cd3916e0c6f72f19a3675a62a60a617
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2018-04-25 23:58:59 +0100

    - Added the ability to for a Collider to hide walls infront of them.

---
+++ Assets/HideModelsTriggerEnter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideModelsTriggerEnter : MonoBehaviour {
+
+    [SerializeField]
+    string hideTag = "";
+
+    Renderer otherRenderer;
+
+    private void OnTriggerEnter(Collider other) {
+     
+        if (other.gameObject.tag != hideTag) {
+
+            return;
+
+        }
+
+        otherRenderer = other.GetComponent<Renderer>();
+
+        if (otherRenderer == false) {
+
+            return;
+
+        }
+
+        otherRenderer.enabled = false;
+
+    }
+
+    private void OnTriggerExit(Collider other) {
+
+        if (other.gameObject.tag != hideTag) {
+
+            return;
+
+        }
+
+        otherRenderer = other.GetComponent<Renderer>();
+
+        if (otherRenderer == false) {
+
+            return;
+
+        }
+
+        otherRenderer.enabled = true;
+
+    }
+}
