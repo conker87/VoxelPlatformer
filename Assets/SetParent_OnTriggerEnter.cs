c60b2dd7909d80487c4b12189bd8332ce668ded5
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2018-05-09 20:15:47 +0100

    - I've added an elevator, because castles have elevators, right?

---
+++ Assets/SetParent_OnTriggerEnter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetParent_OnTriggerEnter : MonoBehaviour {
+
+    [SerializeField]
+    Transform levelsParent, pivotTransform;
+    Transform player;
+
+	// Use this for initialization
+	void Start () {
+
+        if (levelsParent == null) {
+
+            levelsParent = GameObject.Find("LevelParent").transform;
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (levelsParent == null || pivotTransform == null) {
+
+            return;
+
+        }
+
+        if (other.GetComponentInParent<Player>() != null) {
+
+            player = other.GetComponentInParent<Player>().transform;
+
+            player.parent = pivotTransform;
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+
+        if (levelsParent == null || pivotTransform == null) {
+
+            return;
+
+        }
+
+        if (other.GetComponentInParent<Player>() != null) {
+
+            player = other.GetComponentInParent<Player>().transform;
+
+            player.parent = levelsParent;
+
+        }
+    }
+}
